abb67151f020a03880193356e82b6c998b74864f
daon1213 <diaeiyo1213@naver.com>
2022-04-20 20:11:14 +0900

    MainActivity commit!

--- app/src/main/java/com/daon/search_map_part4_03/MainActivity.kt
+++ app/src/main/java/com/daon/search_map_part4_03/MainActivity.kt
@@ -2,9 +2,12 @@ package com.daon.search_map_part4_03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.view.isVisible
 import com.daon.search_map_part4_03.adapter.SearchRecyclerAdapter
 import com.daon.search_map_part4_03.databinding.ActivityMainBinding
+import com.daon.search_map_part4_03.model.LocationLatLngEntity
+import com.daon.search_map_part4_03.model.SearchResultEntity
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +21,8 @@ class MainActivity : AppCompatActivity() {
 
         initAdapter()
         initViews()
+        initData()
+        setData()
     }
 
     private fun initViews() = with(binding) {
@@ -27,5 +32,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun initAdapter() {
         adapter = SearchRecyclerAdapter()
+    }
+
+    private fun initData() {
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun setData() {
+        val dataList = (0..10).map {
+            SearchResultEntity(
+                buildingName = "빌딩 $it",
+                fullAdress = "주소 $it",
+                locationLatLng = LocationLatLngEntity(
+                    it.toFloat(),
+                    it.toFloat()
+                )
+            )
+        }
+        adapter.setSearchResultList(dataList) {
+            Toast.makeText(this, "빌딩이름 : ${it.buildingName} 주소  ${it.fullAdress}", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
