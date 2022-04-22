002608adca2e7fd5a2e4ba0a82671a62210c1a25
daon1213 <diaeiyo1213@naver.com>
2022-04-22 14:36:39 +0900

    MapActivity commit!

--- app/src/main/java/com/daon/search_map_part4_03/MapActivity.kt
+++ app/src/main/java/com/daon/search_map_part4_03/MapActivity.kt
@@ -3,14 +3,28 @@ package com.daon.search_map_part4_03
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.daon.search_map_part4_03.databinding.ActivityMapBinding
+import com.daon.search_map_part4_03.model.SearchResultEntity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback{
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityMapBinding
+    private lateinit var map: GoogleMap
+    private var currentSelectMarker: Marker? = null
+
+    private lateinit var searchResult: SearchResultEntity
+
+    companion object {
+        const val SEARCH_RESULT_EXTRA_KEY = "SEARCH_RESULT_EXTRA_KEY"
+        const val CAMERA_ZOOM_LEVEL = 17f
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,14 +32,39 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback{
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setUpGoogleMap()
+        if (::searchResult.isInitialized.not()) {
+            intent?.let {
+                searchResult = it.getParcelableExtra(SEARCH_RESULT_EXTRA_KEY)
+                    ?: throw Exception("데이터가 존재하지 않습니다.")
+                setUpGoogleMap()
+            }
+        }
     }
+
     private fun setUpGoogleMap() {
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    override fun onMapReady(p0: GoogleMap) {
+    override fun onMapReady(map: GoogleMap) {
+        this.map = map
+        currentSelectMarker = (setUpMarker(searchResult) ?: return) as Marker
+        currentSelectMarker!!.showInfoWindow()
+    }
 
+    private fun setUpMarker(searchResult: SearchResultEntity): Marker? {
+        val positionLatLng = LatLng(
+            searchResult.locationLatLng.latitude.toDouble(),
+            searchResult.locationLatLng.longitude.toDouble()
+        )
+        val markerOptions = MarkerOptions().apply {
+            position(positionLatLng)
+            title(searchResult.buildingName)
+            snippet(searchResult.fullAdress)
+        }
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(positionLatLng, CAMERA_ZOOM_LEVEL))
+
+        return map.addMarker(markerOptions)
     }
 }
